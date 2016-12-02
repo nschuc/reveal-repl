402b1f89613fccbb812057a95eb105de260dc499
Nathan Schucher <nathan.schucher@gmail.com>
2016-12-02 02:03:17 -0500

    Adds evaluation for each line inline after comment

--- repl.js
+++ repl.js
@@ -10,20 +10,43 @@ function evalCode(slide) {
 		obj.__proto__ = proto;
 	};
 
-	if (code && echo) {
-		try {
-			echo.textContent = eval(code.textContent);
-		} catch (error) {
-			echo.textContent = error;
-		} finally {
-			hljs.highlightBlock(echo);
-		}
+	if (code) {
+      var content = code.textContent.split('\n');
+      console.log(code.textContent);
+      code.textContent = "";
+      for(let line of content){
+        line = line.split("//")[0].trim();
+        var output = "";
+        try {
+          output = eval(line);
+        } catch (error) {
+          output = error;
+        }
+        code.textContent += line;
+        code.textContent += output ? " // " + output : "";
+        code.textContent += "\n";
+      }
+      hljs.highlightBlock(code);
+      setCaret(code);
 	}
 }
 
+function setCaret(code) {
+  code.focus();
+  var caret = code.textContent.length - 1; // insert caret after the 10th character say
+  var range = document.createRange();
+  range.setStart(code, caret);
+  range.setEnd(code, caret);
+  var sel = window.getSelection();
+  sel.removeAllRanges();
+  sel.addRange(range);
+}
+
 document.addEventListener('keydown', function(event) {
-	if (event.which == 119)		  // F8
+	if (event.which == 13) {
 		evalCode(Reveal.getCurrentSlide());
+    e.preventDefault();
+  }
 
 	// Hitting Page Up or Page Down when in a content editable box messes
 	// up the layout.  Fix by disabling these keys.
